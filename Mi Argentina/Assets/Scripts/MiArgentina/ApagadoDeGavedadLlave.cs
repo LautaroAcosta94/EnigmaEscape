f8f73ae7f9f5a4899d77175a5ad91f7a49f0c87d
LautaroAcosta94 <lautarogacosta.1994@gmail.com>
2022-12-18 00:22:04 -0300

    Alpha 0.2 - WIP
    Scripts

--- Mi Argentina/Assets/Scripts/MiArgentina/ApagadoDeGavedadLlave.cs
+++ Mi Argentina/Assets/Scripts/MiArgentina/ApagadoDeGavedadLlave.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApagadoDeGavedad : MonoBehaviour
+public class ApagadoDeGavedadLlave : MonoBehaviour
 {
 
     public float range = 5f;
     public Camera fpsCam;
 
-    bool provinciaEnMano = false;
+    bool llaveEnMano = false;
 
-    // Update is called once per frame
     void Update()
     {
         ApagarGravedad();
@@ -23,28 +22,29 @@ public class ApagadoDeGavedad : MonoBehaviour
 
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-            if(hit.transform.CompareTag("Provincias"))
+            if(hit.transform.CompareTag("Llave"))
             {
                 if(Input.GetMouseButtonDown(0))
                 {
                     gameObject.GetComponent<Rigidbody>().isKinematic = true;
-                    gameObject.transform.localScale = new Vector3(0.14782f, 0.14782f, 0.14782f);
-                    provinciaEnMano = true;
+                    gameObject.transform.localScale = new Vector3(0.0770744f, 0.0770744f, 0.0770744f);
+                    llaveEnMano = true;
                 }
             }
         }
     }
 
+    
     void EncendidoGravedad()
-    {   
-        if(provinciaEnMano == true)
+    {
+        if(llaveEnMano == true)
         {
             if(Input.GetMouseButtonUp(0))
             {
                 gameObject.GetComponent<Rigidbody>().isKinematic = false;
                 gameObject.transform.SetParent(null);
-                gameObject.transform.localScale = new Vector3(0.14782f, 0.14782f, 0.14782f);
-                provinciaEnMano = false;
+                gameObject.transform.localScale = new Vector3(0.0770744f, 0.0770744f, 0.0770744f);
+                llaveEnMano = false;
             }
         }
     }
